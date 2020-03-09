816b7d493f1369c690be54a91793ee6f3da08e95
Liam McAllister <liammca1987@gmail.com>
2020-03-09 16:20:29 +0000

    Finished faq.html / created quote.html

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,5 @@
-let btnToTop = document.getElementById('btn-to-top');
+// Back To Top Button
+const btnToTop = document.getElementById('btn-to-top');
 
 window.addEventListener('scroll', function () {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
@@ -14,3 +15,24 @@ btnToTop.addEventListener('click', function () {
         behavior: 'smooth'
     });
 });
+
+
+// FAQ Accordian
+const faqQuestion = document.querySelectorAll('.question');
+const faqAnswer = document.querySelectorAll('.answer');
+
+for (let i = 0; i < faqAnswer.length; i++) {
+    faqAnswer[i].style.display = 'none';
+}
+
+for (let i = 0; i < faqQuestion.length; i++) {
+    faqQuestion[i].addEventListener('click', function () {
+        this.classList.toggle("active");
+        let answerStatus = this.nextElementSibling;
+        if (answerStatus.style.display === 'block') {
+            answerStatus.style.display = 'none';
+        } else {
+            answerStatus.style.display = 'block';
+        }
+    });
+}
