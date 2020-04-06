4c65322b70a69bdc0f94d0e428e2b63e3b43cea9
liam-mcallister <liammca1987@gmail.com>
2020-04-06 21:22:33 +0100

    Added packing supplies images / embedded google maps

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,26 @@
-// Back To Top Button
+/*------------------------------------Cookie Box----*/
+(function () {
+    if (!localStorage.getItem('cookieAccept')) {
+        document.body.innerHTML += '\
+		<div id="cookie" class="cookie">\
+            <p>We use <a href="cookie.html">cookies</a></p>\
+            <button id="cookieBtn" class="btn">OK</button>\
+		</div>\
+        ';
+
+        function cookieClick(e) {
+            e.preventDefault();
+            document.getElementById('cookie').style.display = 'none';
+            localStorage.setItem('cookieAccept', true);
+        }
+
+        document.getElementById('cookieBtn').addEventListener('click', cookieClick, false);
+    }
+})();
+
+
+
+/*----------------------------Back To Top Button----*/
 const btnToTop = document.getElementById('btn-to-top');
 
 window.addEventListener('scroll', function () {
@@ -17,7 +39,8 @@ btnToTop.addEventListener('click', function () {
 });
 
 
-// FAQ Accordian
+
+/*---------------------------------FAQ Accordian----*/
 const faqQuestion = document.querySelectorAll('.question');
 const faqAnswer = document.querySelectorAll('.answer');
 
@@ -37,15 +60,41 @@ for (let i = 0; i < faqQuestion.length; i++) {
     });
 }
 
-// Size Guide Tabs
+
+
+/*-----------------------------------Google Maps----*/
+function initMap() {
+    const belfast = { lat: 54.607868, lng: -5.926437 };
+    const map = new google.maps.Map(document.getElementById('contact-map'), {
+        zoom: 15,
+        center: belfast
+    });
+    const marker = new google.maps.Marker({ position: belfast, map: map });
+};
+
+
+
+
+
+
+
+
+
+
+
+/*------------------------------------Size Guide----*/
 function tabClick(e) {
-    const paneActive = document.querySelectorAll('.pane-active');
+    let paneActive = document.querySelectorAll('.pane-active');
     for (let i = 0; i < paneActive.length; i++) {
         paneActive[i].className = paneActive[i].className.replace('pane-active', '');
     }
     e.target.parentElement.className += ' pane-active';
     document.getElementById(e.target.href.split('#')[1]).className += ' pane-active';
-}
+};
 
-let tabNav = document.getElementById('tab-nav');
+const tabNav = document.getElementById('tab-nav');
 tabNav.addEventListener('click', tabClick, false);
+
+
+
+/*-------------------------Quote Form Validation----*/
